76d8e036166172c2dcfe2563704177ded9fa9699
Prabhu-raj <prabhurajpudi@gmail.com>
2025-08-13 00:54:43 +0530

    Aug 13

--- src/features/Contact_Us.feature
+++ src/features/Contact_Us.feature
@@ -1,11 +1,59 @@
 Feature: WebdriverUniversity.com - Contact Us Page
-    Scenario: Valid Contact Us Form Submission
+    # Scenario: Valid Contact Us Form Submission
+    #     Given I navigate to the WebdriverUniversity homepage
+    #     When I click on the Contact us button
+    #     And I switch to the new browser tab
+    #     And I type a First name
+    #     And I type a Last name
+    #     And I enter an Email address
+    #     And I type a Comment
+    #     And I click on the Submit button
+    #     Then I should be presented with a successful contact us Submission message
+
+    # Scenario: Invalid Contact Us Form Submission
+    #     Given I navigate to the WebdriverUniversity homepage
+    #     When I click on the Contact us button
+    #     And I switch to the new browser tab
+    #     And I type a First name
+    #     And I type a Last name
+    #     # And I enter an Email address
+    #     And I type a Comment
+    #     And I click on the Submit button
+    #     Then I should be presented with a unsuccessful contact us Submission message
+
+    # Scenario: Valid Contact Us Form Submission - Using Specific Data
+    #     Given I navigate to the WebdriverUniversity homepage
+    #     When I click on the Contact us button
+    #     And I switch to the new browser tab
+    #     And I type a specific First name "Prabhu"
+    #     And I type a specific Last name "Raj"
+    #     And I enter a specific Email address "prabhu@example.com"
+    #     And I type specific text "Hello world" and a number 2 within the comment input field
+    #     And I click on the Submit button
+    #     Then I should be presented with a successful contact us Submission message
+
+    # Scenario: Valid Contact Us Form Submission - Using Random data
+    #     Given I navigate to the WebdriverUniversity homepage
+    #     When I click on the Contact us button
+    #     And I switch to the new browser tab
+    #     And I type a random First name
+    #     And I type a random Last name
+    #     And I enter a random Email address
+    #     And I type a Comment
+    #     And I click on the Submit button
+    #     Then I should be presented with a successful contact us Submission message
+
+    Scenario Outline: Validate Contact Us Page
         Given I navigate to the WebdriverUniversity homepage
         When I click on the Contact us button
         And I switch to the new browser tab
-        And I type a First name
-        And I type a Last name
-        And I enter an Email address
-        And I type a Comment
+        And I Type a First name <firstName> and a Last name <lastName>
+        And I type a Email address '<emailAddress>' and a comment '<comment>'
         And I click on the Submit button
-        Then I should be presented with a successful contact us Submission message
+        Then I should be Presented with header text '<message>'
+
+        Examples:
+            | firstName | lastName | emailAddress              | comment                 | message                     |
+            | John      | Jones    | john_jones@example.com    | Hello world?            | Thank You for your Message! |
+            | Mia       | Carter   | mia_carter123@example.com | Test123 Test123         | Thank You for your Message! |
+            | Grace     | Hudson   | grace_hudson              | Do you create websites? | Error: Invalid email address|
--- src/step-definitions/contactUs-Steps.ts
+++ src/step-definitions/contactUs-Steps.ts
@@ -1,7 +1,8 @@
-import { Given, When, Then } from "@cucumber/cucumber";
+import { Given, When, Then, World } from "@cucumber/cucumber";
 import { page, context, browser } from "./Base_file";
 import { pageFixture } from "../hooks/browserContextFixture";
 import { expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
 
 
 When("I type a First name", async () => {
@@ -56,3 +57,77 @@ Then('I should be presented with a successful contact us Submission message', as
         expect(text).toBe('Thank You for your Message!')
         // await page.instance!.pause();
         });
+
+Then('I should be presented with a unsuccessful contact us Submission message', async () => {
+           // Waiting for the header text element
+        await page.instance!.waitForSelector('body', {timeout: 60000});
+
+        const bodyText = await page.instance?.innerText('body')
+
+        expect(bodyText).toMatch(/Error: all fields are required | Invalid email address/)
+
+        });
+
+
+// Cucumber expressions
+When("I type a specific First name {string}", async (firstName: string) => {
+    const fillFirstName = page.instance!.getByRole("textbox", { name: "First Name" });
+    await fillFirstName.fill(firstName);
+})
+
+When("I type a specific Last name {word}", async (lastName: string) => {
+    const fillLastName = page.instance!.getByRole('textbox', { name: 'Last Name' });
+    await fillLastName.fill(lastName)
+})
+
+When("I enter a specific Email address {string}", async (email: string) => {
+    const fillEmail = page.instance!.getByRole('textbox', { name: 'Email Address' });
+    await fillEmail.fill(email)
+})
+
+When("I type specific text {string} and a number {int} within the comment input field", async (text: string, number: number) => {
+    const fillComment = page.instance!.getByRole('textbox', { name: 'Comments' });
+    await fillComment.fill(`${text} ${number}`);
+})
+
+// Random data Faker
+When("I type a random First name", async () => {
+    const randomFirstName = faker.person.firstName();
+    await page.instance!.getByRole("textbox", { name: "First Name" }).fill(randomFirstName);
+})
+
+When("I type a random Last name", async () => {
+    const randomLastName = faker.person.lastName();
+    await page.instance!.getByRole('textbox', { name: 'Last Name' }).fill(randomLastName);
+})
+
+When("I enter a random Email address", async () => {
+    const randomEmail = faker.internet.email();
+    await page.instance!.getByRole('textbox', { name: 'Email Address' }).fill(randomEmail);
+})
+
+// scrnario outlines
+When("I Type a First name {word} and a Last name {word}", async (firstName: string, lastName: string) => {
+    await page.instance!.getByRole("textbox", { name: "First Name" }).fill(firstName);
+    await page.instance!.getByRole('textbox', { name: 'Last Name' }).fill(lastName);
+})
+
+When("I type a Email address {string} and a comment {string}", async (email: string, comment: string) => {
+    await page.instance!.getByRole('textbox', { name: 'Email Address' }).fill(email);
+    await page.instance!.getByRole('textbox', { name: 'Comments' }).fill(comment);
+})
+
+
+Then("I should be Presented with header text {string}", async (headerText: string) => {
+    // Combine selectors with XPath OR
+    const locator = page.instance!.locator('//h1 | //body');
+
+    // Wait for either element to appear
+    await locator.first().waitFor({ state: "visible" });
+
+    // Get the visible element's text
+    const actualText = (await locator.first().innerText()).trim();
+
+    // Validate the text
+    expect(actualText).toBe(headerText);
+});
